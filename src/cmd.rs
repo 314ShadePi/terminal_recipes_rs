068ac9b3bacc40a83344cb7d1e1fd7ffb999a97e
314ShadePi <erikkirkegaardstage@gmail.com>
2023-02-15 13:53:08 +0100

    cmd + .recipes/

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,16 +1,64 @@
 use std::str::FromStr;
 
+#[derive(Debug, Eq, PartialEq)]
 pub enum Cmd {
     Exit,
+    List,
+    View(String),
 }
 
 impl FromStr for Cmd {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = match s.contains(" ") {
+            true => match s.split_once(' ') {
+                None => return Err(()),
+                Some(s) => s,
+            },
+            false => (s, ""),
+        };
+
         match s {
-            "exit" => Ok(Self::Exit),
+            ("exit", _) => Ok(Self::Exit),
+            ("list", _) => Ok(Self::List),
+            ("view", param) => Ok(Self::View(param.to_string())),
             _ => Err(()),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn cmd_from_str_exit() {
+        let exit = Cmd::from_str("exit").unwrap();
+        assert_eq!(exit, Cmd::Exit);
+    }
+
+    #[test]
+    fn cmd_from_str_list() {
+        let list = Cmd::from_str("list").unwrap();
+        assert_eq!(list, Cmd::List);
+    }
+
+    #[test]
+    fn cmd_from_str_view_no_params() {
+        let view_1 = Cmd::from_str("view").unwrap();
+        assert_eq!(view_1, Cmd::View("".to_string()));
+    }
+
+    #[test]
+    fn cmd_from_str_view_one_param() {
+        let view_2 = Cmd::from_str("view qwert").unwrap();
+        assert_eq!(view_2, Cmd::View("qwert".to_string()));
+    }
+
+    #[test]
+    fn cmd_from_str_view_two_params() {
+        let view_3 = Cmd::from_str("view qwert yuiop").unwrap();
+        assert_eq!(view_3, Cmd::View("qwert yuiop".to_string()));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,40 @@ mod cmd;
 mod recipes;
 
 use crate::cmd::Cmd;
+use home::home_dir;
 use inquire::Text;
+use std::fs::create_dir;
 use std::str::FromStr;
 
 fn main() {
+    let home = match home_dir() {
+        None => {
+            println!("Couldn't get home_dir.");
+            std::process::exit(1);
+        }
+        Some(h) => h,
+    };
+
+    let recipe_dir = home.join(".recipes/");
+
+    match recipe_dir.try_exists() {
+        Ok(res) => {
+            if !res {
+                match create_dir(recipe_dir) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("ERROR: {}", e);
+                        std::process::exit(1);
+                    }
+                }
+            }
+        }
+        Err(e) => {
+            println!("ERROR: {}", e);
+            std::process::exit(1);
+        }
+    }
+
     loop {
         let cmd = Text::new("Terminal Recipes> ").prompt();
 
@@ -28,5 +58,7 @@ fn handle_cmd(cmd: String) {
 
     match cmd {
         Cmd::Exit => std::process::exit(0),
+        Cmd::List => {}
+        Cmd::View(_) => {}
     }
 }
