62126cada40e4dcc03460bf6dbfa1942e1fb5213
Purple CRAFT <s3llen3@gmail.com>
2023-02-21 10:13:11 +0100

    Config cmd

---
+++ lib/terminal_recipes_rs_lib/src/lib.rs
@@ -0,0 +1,4 @@
+mod config_options;
+
+pub use terminal_recipes_rs_macro::ConfigOptions;
+pub use crate::config_options::ConfigOptions;
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,3 +1,4 @@
 pub mod list;
 pub mod rebuild_cache;
 pub mod view;
+pub mod config;
